1493fcf2f4cab02d57330d3a79a654d7959aa892
Maxim Baz <git@maximbaz.com>
2015-10-07 14:54:47 +0200

    [zsh] Add git aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -16,6 +16,8 @@ alias gdh="gd HEAD"
 alias gdw="gd --color-words"
 alias gdwh="gd --color-words HEAD"
 alias glog="git log --graph --pretty=format:'%C(yellow)%h%Creset -%C(bold red)%d%Creset %s %C(dim green)(%cr) %C(cyan)<%cn>%Creset' --abbrev-commit"
+alias gr="git reset"
+alias gs="git show"
 
 # Debian
 alias au="sudo apt-get update && sudo apt-get upgrade"
