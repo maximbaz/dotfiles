521bf807ca968bf20c233540257dae1580a1c092
Maxim Baz <git@maximbaz.com>
2016-04-05 01:58:02 +0200

    [bin] Always attempt to use "us-da" layout for English first

--- bin/change-keyboard-layout.sh
+++ bin/change-keyboard-layout.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
 reset=$1
+current="$(setxkbmap -query | grep layout | perl -pe 's/^layout: +([^ ]+)$/$1/')"
 
-if [ "$reset" != "" ]; then
-  setxkbmap -layout "us"
-else
-  current="$(setxkbmap -query | grep layout | perl -pe 's/^layout: +([^ ]+)$/$1/')"
-  if [ "$current" = "us" ] ; then
-    setxkbmap -layout "ru"
-  else
-    setxkbmap -layout "us" -variant "us-da"
-    if [ $? != 0 ]; then
-      setxkbmap -layout "us"
-    fi
+if [ "$reset" != "" ] || [ "$current" != "us" ]; then
+  setxkbmap -layout "us" -variant "us-da"
+  if [ $? != 0 ]; then
+    setxkbmap -layout "us"
   fi
+else
+  setxkbmap -layout "ru"
 fi
 
 # Refresh py3status immediately
