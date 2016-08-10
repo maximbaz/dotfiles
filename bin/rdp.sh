544e0173955ab3f1ef3373cec71e887b210548eb
Maxim Baz <git@maximbaz.com>
2016-08-10 23:18:48 +0200

    [rdp] Move params to a file

--- bin/rdp.sh
+++ bin/rdp.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# PARAMETERS
-MONITOR=${1:-0}
-MACHINE=${2:-CRMDevVM-0024}
-USER=${3:-maximbaz}
-DOMAIN=${4:-europe}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/rdp.conf
 
-xfreerdp /f +fonts +aero +window-drag +menu-anims /bpp:32 /gdi:hw /gfx +gfx-progressive /sound:sys:pulse /microphone:sys:pulse +clipboard /cert-ignore +auto-reconnect /u:$USER /d:$DOMAIN /v:$MACHINE /monitors:$MONITOR
+xfreerdp /f +fonts +aero +window-drag +menu-anims /bpp:32 /gdi:hw /gfx +gfx-progressive /sound:sys:pulse /microphone:sys:pulse +clipboard /cert-ignore +auto-reconnect /u:$USER /d:$DOMAIN /v:$MACHINE /p:$PASSWORD
 
