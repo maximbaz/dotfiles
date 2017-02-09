db434e2a35f27a553ab5e9b80fe15daeacd0d5b1
Maxim Baz <git@maximbaz.com>
2017-02-10 00:42:36 +0100

    [bin] Fix screen repain on RDP

--- bin/rdp.sh
+++ bin/rdp.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/rdp.conf
 
-xfreerdp /f +fonts +aero +window-drag +menu-anims /bpp:32 /gdi:hw /gfx +gfx-progressive /sound:sys:pulse /microphone:sys:pulse +clipboard /cert-ignore +auto-reconnect /u:$USER /d:$DOMAIN /v:$MACHINE /p:$PASSWORD
+xfreerdp /f /fonts /aero /window-drag /menu-anims /bpp:32 /gdi:sw /gfx /gfx-progressive /sound:sys:pulse /clipboard /cert-ignore /auto-reconnect /auto-reconnect-max-retries:3 /u:$USER /d:$DOMAIN /v:$MACHINE /p:$PASSWORD
 
