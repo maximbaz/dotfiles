d32f61cd46480a08c0d3d4423bc1920a8ccc95b9
Maxim Baz <git@maximbaz.com>
2017-04-15 14:08:25 +0200

    [bin] Control brightness with precision

---
+++ bin/set-brightness.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo -e "Usage examples:\n$0 inc\n$0 dec" >&2
+  exit 1
+fi
+
+printf -v current %.0f $(xbacklight)
+if [ "$1" == "dec" ]; then
+  if (( current <= 5 )); then
+    increment=-1
+  else
+    increment=-5
+  fi
+else
+  if (( current < 5 )); then
+    increment=1
+  else
+    increment=5
+  fi
+fi
+
+new=$(( current + increment ))
+xbacklight -set $new
