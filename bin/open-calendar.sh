0bdac62b43f9082aa311a28a85d26651128c05c1
Maxim Baz <git@maximbaz.com>
2017-04-24 18:57:51 +0200

    [bin] Better wait for calendar window to open before positioning

--- bin/open-calendar.sh
+++ bin/open-calendar.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 gsimplecal &
-sleep 0.05 # let the window open
+
+# let the window open
+while [ $(xdotool search --class Gsimplecal | wc -l) -lt 2 ]; do
+  sleep 0.01
+done
 
 screen_width=$(xrandr | grep current | sed -r 's/^.*current ([0-9]+).*/\1/')
 x_pos=$(( screen_width - 620 ))
-y_pos=37
+y_pos=38
 
 i3-msg "[class=\"Gsimplecal\"] move absolute position $x_pos $y_pos"
