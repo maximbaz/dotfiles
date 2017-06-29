def6e0f97ca1ef4082071f1871ae7b747f3a5a2e
Maxim Baz <git@maximbaz.com>
2017-06-30 01:37:38 +0200

    [bin] Update positioning of the calendar (due to DPI changes)

--- bin/open-calendar.sh
+++ bin/open-calendar.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 gsimplecal &
 
@@ -8,7 +8,7 @@ while [ $(xdotool search --class Gsimplecal | wc -l) -lt 2 ]; do
 done
 
 screen_width=$(xrandr | grep current | sed -r 's/^.*current ([0-9]+).*/\1/')
-x_pos=$(( screen_width - 620 ))
-y_pos=38
+x_pos=$(( screen_width - 750 ))
+y_pos=45
 
 i3-msg "[class=\"Gsimplecal\"] move absolute position $x_pos $y_pos"
