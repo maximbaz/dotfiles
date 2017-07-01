ee963150e753c456324500f768f765dfc5eead2b
Maxim Baz <git@maximbaz.com>
2017-07-01 21:47:52 +0200

    [bin] Script to help chanding DPI values

---
+++ bin/dpi-set.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+display=$(xrandr --listactivemonitors | grep '0:' | grep -o '[^ ]*$')
+
+if [ "$#" -eq 1 ]; then
+  dpi="$1"
+elif [[ "$display" == "eDP-1" ]]; then
+  dpi=240
+else
+  dpi=192
+fi
+
+echo "Setting DPI=$dpi"
+
+sed -i --follow-symlinks "s/\(xrandr --dpi\) [0-9]\+/\1 $dpi/" ~/.xinitrc
+sed -i --follow-symlinks "s/\(Xft.dpi:\) [0-9]\+/\1 $dpi/" ~/.Xresources
+sed -zi --follow-symlinks "s/\(dpi:\n\s*x:\) [0-9]\+\(\.0\n\s*y:\) [0-9]\+/\1 $dpi\2 $dpi/" ~/.config/alacritty/alacritty.yml
