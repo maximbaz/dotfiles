1c2d84362a86d0d33c5c61b969d4d4d5fff4b1ff
Maxim Baz <git@maximbaz.com>
2017-11-17 23:31:33 +0100

    [bin] Update pacman bootstrap script

--- bin/pacman-bootstrap.sh
+++ bin/pacman-bootstrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env zsh
 
-if [[ "$1" != "native" && "$1" != "foreign" ]]; then
-  echo "Usage: $0 <native|foreign>"
+if [[ "$1" != "pacman" && "$1" != "aur" ]]; then
+  echo "Usage: $0 <pacman|aur>"
   exit 1
 fi
 
 zmodload -ap zsh/mapfile mapfile
-packages=( ${(f)mapfile[/home/maximbaz/lib/pacman.$1.list]} )
+packages=( ${(f)mapfile[/home/maximbaz/.dotfiles/packages/pacman.$1.list]} )
 
-if [[ "$1" == "native" ]]; then
+if [[ "$1" == "pacman" ]]; then
   sudo pacman -Sy "$packages[@]"
 else
   yay -S "$packages[@]"
