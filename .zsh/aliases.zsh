3ce444094d6e2b1d46cfba200f72a407f334a1f7
Maxim Baz <git@maximbaz.com>
2017-11-22 03:48:47 +0100

    [zsh] Add missing aliases from removed utility module, use better LS_COLORS

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,16 +1,26 @@
 alias ag='ag --hidden -f'
-alias diff='diff --color'
-alias dragon='dragon --and-exit'
-alias dragall='dragon --and-exit --all'
-alias http-serve='python3 -m http.server'
-alias plz='sudo $(fc -ln -1)'
-alias psg='ps aux | grep '
 alias cp='cp -r'
+alias df='pydf'
+alias diff='diff --color --unified'
+alias dragall='dragon --and-exit --all'
+alias dragon='dragon --and-exit'
+alias e='nvim'
+alias http-serve='python3 -m http.server'
+alias mkdir='mkdir -p'
+alias o='xdg-open'
 alias rm='rmtrash -rf'
-alias rsync='rsync -a --info=progress2'
-alias srm='trash-put'
-alias srml='trash-list'
-alias srmr='trash-restore'
+alias rsync='rsync --verbose --archive --info=progress2 --human-readable --compress'
 alias sudo='sudo -E '
 alias vi='nvim'
 alias vim='nvim'
+
+eval `dircolors -b /usr/share/LS_COLORS`
+alias ls="ls --color=auto --group-directories-first"
+alias ll="ls -lh"
+alias la="ll -A"
+alias lk="ll -Sr"   # Sorted by size
+alias lt="ll -tr"   # Sorted by date
+
+function mkdcd {
+  [[ -n "$1" ]] && mkdir -p "$1" && builtin cd "$1"
+}
