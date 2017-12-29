445eed45a8e7edc96abed0fbca1c1dbdc3d38267
Maxim Baz <git@maximbaz.com>
2017-12-29 16:15:02 +0100

    [py3status] Add module to monitor pacdiff

--- .zsh/pacman.zsh
+++ .zsh/pacman.zsh
@@ -1,7 +1,7 @@
 alias pac='yay -Sy; yay'
 alias paci='yay -Sy'
 alias pacf='yay -U'
-alias pacu='yay -Syu; py3-cmd refresh arch_updates'
+alias pacu='yay -Syu; py3-cmd refresh arch_updates "external_script pacdiff"'
 alias pacr='yay -Rns'
 alias pacq='yay -Si'
 alias pacR='yay -Cd'
