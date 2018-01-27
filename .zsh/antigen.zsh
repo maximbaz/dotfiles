b9b93f8d10cb02916889fe86a48bfbb9ab3dc611
Maxim Baz <git@maximbaz.com>
2018-01-27 20:23:43 +0100

    [zsh] Add binding to toggle comments

--- .zsh/antigen.zsh
+++ .zsh/antigen.zsh
@@ -91,3 +91,4 @@ bindkey -M emacs "$key_info[Up]" history-substring-search-up
 bindkey -M emacs "$key_info[Down]" history-substring-search-down
 bindkey -M emacs "$key_info[Control]K" backward-kill-line
 bindkey -M emacs "$key_info[Control]V$key_info[Control]V" edit-command-line
+bindkey -M emacs "^_" pound-toggle
