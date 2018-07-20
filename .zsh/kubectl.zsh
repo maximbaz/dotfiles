a0734265c5523accb1f5ad01b0fda44835e5b660
Maxim Baz <git@maximbaz.com>
2018-07-21 00:25:56 +0200

    Add kgp! alias

--- .zsh/kubectl.zsh
+++ .zsh/kubectl.zsh
@@ -1,5 +1,6 @@
 alias k='kubectl'
 alias kgp='kubectl get pod'
+alias kgp!='kubectl get pod --all-namespaces'
 alias kl='kubectl logs -f'
 alias kdp='kubectl describe pod'
 alias ke='kubectl exec -ti'
