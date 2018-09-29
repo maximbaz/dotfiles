a928763e6f13f0e23c9b16c76be049dd17c20fde
Maxim Baz <git@maximbaz.com>
2018-09-29 16:17:05 +0200

    Switch to xsecurelock

--- etc/profile.d/zz_custom.sh
+++ etc/profile.d/zz_custom.sh
@@ -9,9 +9,6 @@ export WORDCHARS='*?_.[]~&!#$%^(){}<>'
 # Used by bash-language-server
 export EXPLAINSHELL_ENDPOINT="http://localhost:5000"
 
-# Keep py3status 'do_not_disturb' module in sync with i3 lock and restart
-export PY3STATUS_DND_LOCK="$HOME/.cache/py3status_do_not_disturb.lock"
-
 # My own binaries
 export PATH="$HOME/bin:$PATH"
 
@@ -20,35 +17,30 @@ case "$(hostname)" in
     desktop-*) export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh" ;;
 esac
 
-# Pass configuration
+export XSECURELOCK_FONT="-*-open sans-medium-r-*-*-30-*-*-*-*-*-*-uni"
+export XSECURELOCK_SHOW_HOSTNAME=0
+export XSECURELOCK_SHOW_USERNAME=0
+export XSECURELOCK_WANT_FIRST_KEYPRESS=1
+
 export PASSWORD_STORE_CHARACTER_SET='a-zA-Z0-9~!@#$%^&*()-_=+[]{};:,.<>?'
 export PASSWORD_STORE_GENERATED_LENGTH=40
 
-# Java configuration
 export JAVA_HOME="/usr/lib/jvm/java-8-jdk"
+export PATH="$JAVA_HOME/bin:$PATH"
 
-# FZ configuration
 export FZ_CMD=j
 export FZ_SUBDIR_CMD=jj
 
-# Java configuration
-export PATH="$JAVA_HOME/bin:$PATH"
-
-# Android configuration
 export ANDROID_SDK_ROOT="$HOME/.android/sdk"
 
-# Haskell configuration
 export PATH="$HOME/.cabal/bin:$PATH"
 
-# Ruby configuration
 if hash ruby 2>/dev/null; then
   export GEM_HOME="$(ruby -e 'print Gem.user_dir')"
   export PATH="$GEM_HOME/bin:$PATH"
 fi
 
-# Go configuration
 export GOPATH="$HOME/.go"
 export PATH="$GOPATH/bin:$PATH"
 
-# NPM configuration
 export PATH="$HOME/.node_modules/bin:$PATH"
