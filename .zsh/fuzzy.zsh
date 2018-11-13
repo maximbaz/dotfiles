e2d20e4fe2774004ea0638a1b69c812c31554d72
Maxim Baz <git@maximbaz.com>
2018-11-13 22:31:13 +0100

    zsh: better fzy integration

--- .zsh/fuzzy.zsh
+++ .zsh/fuzzy.zsh
@@ -1,4 +1,8 @@
-bindkey '^T'  fzy-file-widget
-bindkey '^R'  fzy-history-widget
+bindkey '^T' fzy-file-widget
+bindkey '^R' fzy-history-widget
+bindkey '^P' fzy-proc-widget
 
-zstyle :fzy:file command rg --files --hidden --smart-case 2>/dev/null
+fzy-file-widget-list-files() { rg --files --hidden --smart-case 2>/dev/null }
+zstyle :fzy:file command fzy-file-widget-list-files
+
+__fz_filter() { fzy }
