494ec93baa63dc7d2bb385013e9f9fd5b9276903
Maxim Baz <git@maximbaz.com>
2020-02-25 23:44:34 +0100

    firefox: better newtab page color

--- .mozilla/firefox/profile/user.js
+++ .mozilla/firefox/profile/user.js
@@ -4,3 +4,6 @@ user_pref("browser.startup.page", 3);
 user_pref("general.smoothScroll", false);
 user_pref("signon.rememberSignons", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.display.background_color", "#282828");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.startup.homepage", "about:blank");
