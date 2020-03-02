893de9bfaa2948b7c1a99245b056813b04570d4e
Maxim Baz <git@maximbaz.com>
2020-03-02 15:39:04 +0100

    firefox: revert default colors change, too many sites broke

--- .mozilla/firefox/profile/user.js
+++ .mozilla/firefox/profile/user.js
@@ -4,7 +4,5 @@ user_pref("browser.startup.page", 3);
 user_pref("general.smoothScroll", false);
 user_pref("signon.rememberSignons", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.display.background_color", "#282828");
-user_pref("browser.display.foreground_color", "#ebdbb2");
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.startup.homepage", "about:blank");
