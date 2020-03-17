858f6f938938d5abc130db5cc553b7681d38d0bd
Maxim Baz <git@maximbaz.com>
2020-03-17 23:14:11 +0100

    firefox: update flags

--- .mozilla/firefox/profile/user.js
+++ .mozilla/firefox/profile/user.js
@@ -6,3 +6,5 @@ user_pref("signon.rememberSignons", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.startup.homepage", "about:blank");
+user_pref("extensions.pocket.enabled", false);
+user_pref("widget.wayland_vsync.enabled", true);
