cd0de4f0c28802193eda8d895393acc39903564c
Maxim Baz <git@maximbaz.com>
2020-03-19 17:39:52 +0100

    zsh: fix fzf-tab completion for executables

--- .zsh/fuzzy.zsh
+++ .zsh/fuzzy.zsh
@@ -5,8 +5,6 @@ fzf_default_completion=z4h-expand-or-complete-with-dots  # ctrl-t falls back to 
 bindkey -r '^[c'                                         # remove unwanted binding
 
 FZF_TAB_PREFIX=                                          # remove '·'
-FZF_TAB_SHOW_GROUP=brief                                 # show group headers only for duplicate options
-FZF_TAB_SINGLE_GROUP=()                                  # no colors and no header for a single group
 FZF_TAB_CONTINUOUS_TRIGGER='alt-enter'                   # alt-enter to accept and trigger another completion
 bindkey '\t' expand-or-complete                          # fzf-tab reads it during initialization
 
