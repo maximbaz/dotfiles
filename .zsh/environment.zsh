38a8823c88c4f9b04b6b39ae625a98054e82b0ba
Maxim Baz <git@maximbaz.com>
2020-03-22 13:40:21 +0100

    move CLI-only env vars to zsh

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -13,5 +13,25 @@ ZSH_HIGHLIGHT_MAXLENGTH=1024                 # don't colorize long command lines
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)   # main syntax highlighting plus matching brackets
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1              # disable a very slow obscure feature
 
+export EDITOR='kak'
+export VISUAL='kak'
+export DIFFPROG='meld'
+export MANPAGER='kak-man-pager'
+export AUR_PAGER='nnn -Aer'
+export WORDCHARS='*?_.[]~&!#$%^(){}<>'
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
+
+export PASSWORD_STORE_CHARACTER_SET='a-zA-Z0-9~!@#$%^&*()-_=+[]{};:,.<>?'
+export PASSWORD_STORE_GENERATED_LENGTH=40
+
+export GOPATH="$HOME/.go"
+export GOPRIVATE="bitbucket.org/maersk-analytics"
+
+export NNN_TRASH=1
+export NNN_COLORS='4235'
+export NNN_PLUG='j:jump;r:remove;c:croc;d:dragdrop;'
+export NNN_BMS='d:~/Downloads;n:/home/nzbget/dst;N:/home/nzbget/nzb;r:/run/media/maximbaz;'
+
 fpath+=~/.zsh-plugins/archive
 autoload -Uz archive lsarchive unarchive
--- etc/profile.d/zz_custom.sh
+++ etc/profile.d/zz_custom.sh
@@ -1,30 +1,7 @@
 #!/bin/sh
 
-export EDITOR='kak'
-export VISUAL='kak'
-export DIFFPROG='meld'
-export MANPAGER='kak-man-pager'
-export AUR_PAGER='nnn -Aer'
-export WORDCHARS='*?_.[]~&!#$%^(){}<>'
-
 export PATH="$HOME/bin:$PATH:/usr/share/sway/scripts/"
-
-# Use gpg-agent as ssh-agent
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
-
-export NNN_TRASH=1
-export NNN_COLORS='4235'
-export NNN_PLUG='j:jump;r:remove;c:croc;d:dragdrop;'
-export NNN_BMS='d:~/Downloads;n:/home/nzbget/dst;N:/home/nzbget/nzb;r:/run/media/maximbaz;'
-
-export PASSWORD_STORE_CHARACTER_SET='a-zA-Z0-9~!@#$%^&*()-_=+[]{};:,.<>?'
-export PASSWORD_STORE_GENERATED_LENGTH=40
-
 export LIBVA_DRIVER_NAME=iHD
-
-export GOPATH="$HOME/.go"
-export GOPRIVATE="bitbucket.org/maersk-analytics"
-
 export MOZ_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland-egl
 export QT_WAYLAND_FORCE_DPI=physical
