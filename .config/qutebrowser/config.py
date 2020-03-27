ecbfde808e667745fcc59b2ff49c32c12cd4170c
Maxim Baz <git@maximbaz.com>
2020-03-28 00:50:25 +0100

    qutebrowser: add

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,49 @@
+# ui
+config.source("gruvbox.py")
+c.colors.webpage.prefers_color_scheme_dark = True
+c.completion.shrink = True
+c.completion.use_best_match = True
+c.fonts.default_size = "7pt"
+c.statusbar.widgets = ["progress", "keypress", "url", "history"]
+c.scrolling.bar = "always"
+c.tabs.position = "left"
+c.tabs.width = "15%"
+c.tabs.title.format_pinned = "{audio}{index}: {current_title}"
+
+# general
+c.auto_save.session = True
+c.content.autoplay = False
+c.content.default_encoding = "utf-8"
+c.content.javascript.can_access_clipboard = True
+c.content.pdfjs = True
+c.editor.command = ["kitty", "kak", "-e", "exec {line}g{column0}l", "{file}"]
+c.downloads.location.prompt = False
+c.input.insert_mode.auto_load = True
+c.tabs.background = True
+c.tabs.last_close = "close"
+c.tabs.mousewheel_switching = False
+
+# privacy
+c.content.cookies.accept = "no-3rdparty"
+c.content.webrtc_ip_handling_policy = "default-public-interface-only"
+c.content.site_specific_quirks = False
+c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+
+# urls
+c.url.searchengines = {
+    "DEFAULT": "https://google.com/search?q={}",
+    "?": "https://google.com/search?q={}",
+}
+c.url.default_page = "~/.config/qutebrowser/blank.html"
+c.url.start_pages = ["~/.config/qutebrowser/blank.html"]
+
+# keys
+bindings = {
+    ",m": "spawn --userscript view_in_mpv",
+    ",M": "hint links spawn mpv {hint-url}",
+    ",p": "spawn --userscript qute-pass --dmenu-invocation dmenu",
+    ",P": "spawn --userscript qute-pass --dmenu-invocation dmenu --password-only",
+}
+
+for key, bind in bindings.items():
+    config.bind(key, bind)
