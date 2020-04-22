5bc5bc5d0724003f85dc6a9ad3db8c74670a29c1
Maxim Baz <git@maximbaz.com>
2020-04-22 22:57:57 +0200

    qutebrowser: analyze all chromium flags, keep only relevant one

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -23,17 +23,7 @@ c.spellcheck.languages = ["en-US", "da-DK"]
 c.tabs.background = True
 c.tabs.last_close = "close"
 c.tabs.mousewheel_switching = False
-c.qt.args += [
-    "ignore-gpu-blacklist",
-    "enable-accelerated-2d-canvas",
-    "enable-devtools-experiments",
-    "enable-gpu-memory-buffer-video-frames",
-    "enable-gpu-rasterization",
-    "enable-native-gpu-memory-buffers",
-    "enable-oop-rasterization",
-    "enable-oop-rasterization-ddl",
-    "enable-zero-copy",
-]
+c.qt.args += ["enable-gpu-rasterization"]
 
 # per-domain settings
 config.set("content.register_protocol_handler", True, "*://calendar.google.com")
