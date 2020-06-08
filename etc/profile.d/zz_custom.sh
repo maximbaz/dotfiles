50fce5c25229678b877e58f293af06489fe2212c
Maxim Baz <git@maximbaz.com>
2020-06-08 23:34:15 +0200

    Fix rendering and perf issues on Wayland with Qt 5.15 upgrade

--- etc/profile.d/zz_custom.sh
+++ etc/profile.d/zz_custom.sh
@@ -4,5 +4,4 @@ export PATH="$HOME/bin:$PATH:/usr/share/sway/scripts/"
 export LIBVA_DRIVER_NAME=iHD
 export MOZ_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland-egl
-export QT_WAYLAND_FORCE_DPI=physical
 export NNN_OPTS='Aer'
