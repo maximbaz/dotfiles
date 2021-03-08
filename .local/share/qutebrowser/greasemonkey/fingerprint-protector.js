a6d694f2d4308b963134ad1ba9401ef2a5bb9f97
Maxim Baz <git@maximbaz.com>
2021-03-08 12:10:48 +0100

    qutebrowser: update useragent

--- .local/share/qutebrowser/greasemonkey/fingerprint-protector.js
+++ .local/share/qutebrowser/greasemonkey/fingerprint-protector.js
@@ -6,7 +6,7 @@
   "use strict";
 
   const userAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36";
   const appVersion = userAgent.substr(8);
 
   Object.defineProperty(window.navigator, "userAgent", {
