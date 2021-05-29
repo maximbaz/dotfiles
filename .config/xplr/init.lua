910e9c64a8a51e9ce02435847b18f0b4e2fafb43
Maxim Baz <git@maximbaz.com>
2021-05-29 16:05:01 +0200

    xplr: format config with stylua

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -1,110 +1,98 @@
-version = '0.13.1'
+version = "0.13.1"
 
 -- ui
-xplr.config.general.focus_ui.style.fg = 'Cyan'
+xplr.config.general.focus_ui.style.fg = "Cyan"
 xplr.config.node_types.directory.meta.icon = nil
-xplr.config.node_types.directory.style.fg = 'Blue'
+xplr.config.node_types.directory.style.fg = "Blue"
 xplr.config.node_types.file.meta.icon = nil
-xplr.config.node_types.file.style.add_modifiers = { 'Dim' }
+xplr.config.node_types.file.style.add_modifiers = { "Dim" }
 xplr.config.node_types.symlink.meta.icon = nil
-xplr.config.node_types.symlink.style.sub_modifiers = { 'Italic' }
-
+xplr.config.node_types.symlink.style.sub_modifiers = { "Italic" }
 
 -- keys: default mode
 key = xplr.config.modes.builtin.default.key_bindings.on_key
 
-key.a = key['ctrl-a']
-key['ctrl-f'] = nil
+key.a = key["ctrl-a"]
+key["ctrl-f"] = nil
 key.e = xplr.config.modes.builtin.action.key_bindings.on_key.e
 key.o = xplr.config.modes.builtin.go_to.key_bindings.on_key.x
 
-key['ctrl-a'] = nil
-key['ctrl-w'] = nil
-key['ctrl-r'] = nil
-key['ctrl-i'] = nil
-key['ctrl-o'] = nil
+key["ctrl-a"] = nil
+key["ctrl-w"] = nil
+key["ctrl-r"] = nil
+key["ctrl-i"] = nil
+key["ctrl-o"] = nil
 key.v = nil
 key.V = nil
 
-key['!'] = {
-  help = "shell",
-  messages = {
-    { Call = { command = "zsh", args = {"-i"} } },
-    "ExplorePwdAsync",
-    "PopMode",
-    "Refresh"
-  }
+key["!"] = {
+    help = "shell",
+    messages = {
+        { Call = { command = "zsh", args = { "-i" } } },
+        "ExplorePwdAsync",
+        "PopMode",
+        "Refresh",
+    },
 }
 
 key.D = {
-  help = "disk usage",
-  messages = {
-    { Call = { command = "ncdu" } },
-    "ClearScreen"
-  }
+    help = "disk usage",
+    messages = {
+        { Call = { command = "ncdu" } },
+        "ClearScreen",
+    },
 }
 
 key.n = {
-  help = "go to nzb",
-  messages = {
-    {
-      BashExecSilently = [===[
-        echo 'ChangeDirectory: "/home/nzbget/dst/"' >> "${XPLR_PIPE_MSG_IN:?}"
-      ]===]
-    }
-  }
+    help = "go to nzb",
+    messages = {
+        { BashExecSilently = [[ echo 'ChangeDirectory: "/home/nzbget/dst/"' >> "${XPLR_PIPE_MSG_IN:?}" ]] },
+    },
 }
-
 
 -- keys: action mode
 key = xplr.config.modes.builtin.action.key_bindings.on_key
 
 key.e = nil
 key.l = {
-  help = "logs",
-  messages = {
-    {
-      BashExec = [===[
-        cat -- "${XPLR_PIPE_LOGS_OUT}" | less -+F
-      ]===]
+    help = "logs",
+    messages = {
+        { BashExec = [[ cat -- "${XPLR_PIPE_LOGS_OUT}" | less -+F ]] },
+        "PopMode",
+        "Refresh",
     },
-    "PopMode",
-    "Refresh"
-  }
 }
-
 
 -- keys: delete mode
 key = xplr.config.modes.builtin.delete.key_bindings.on_key
 
 key.d = {
-  help = "delete",
-  messages = {
-    {
-      BashExec = [===[
-        while IFS= read -r line; do rmtrash -rf -- "${line:?}"; done < "${XPLR_PIPE_RESULT_OUT:?}"
-        echo ExplorePwdAsync >> "${XPLR_PIPE_MSG_IN:?}"
-      ]===]
+    help = "delete",
+    messages = {
+        {
+            BashExec = [===[
+                while IFS= read -r line; do rmtrash -rf -- "${line:?}"; done < "${XPLR_PIPE_RESULT_OUT:?}"
+                echo ExplorePwdAsync >> "${XPLR_PIPE_MSG_IN:?}"
+            ]===],
+        },
+        "PopMode",
+        "Refresh",
     },
-    "PopMode",
-    "Refresh"
-  }
 }
 
 key.D = {
-  help = "force delete",
-  messages = {
-    {
-      BashExec = [===[
-        while IFS= read -r line; do rm -rf -- "${line:?}"; done < "${XPLR_PIPE_RESULT_OUT:?}"
-        echo ExplorePwdAsync >> "${XPLR_PIPE_MSG_IN:?}"
-      ]===]
+    help = "force delete",
+    messages = {
+        {
+            BashExec = [===[
+                while IFS= read -r line; do rm -rf -- "${line:?}"; done < "${XPLR_PIPE_RESULT_OUT:?}"
+                echo ExplorePwdAsync >> "${XPLR_PIPE_MSG_IN:?}"
+            ]===],
+        },
+        "PopMode",
+        "Refresh",
     },
-    "PopMode",
-    "Refresh"
-  }
 }
-
 
 -- keys: go to
 key = xplr.config.modes.builtin.go_to.key_bindings.on_key
@@ -113,12 +101,11 @@ key.k = key.g
 key.j = xplr.config.modes.builtin.default.key_bindings.on_key.G
 key.x = nil
 
-
 -- keys: search mode
 key = xplr.config.modes.builtin.search.key_bindings.on_key
 
-key['ctrl-p'] = key['tab']
-key['tab'] = key.right
-key['ctrl-j'] = key.down
-key['ctrl-k'] = key.up
-key['ctrl-n'] = nil
+key["ctrl-p"] = key["tab"]
+key["tab"] = key.right
+key["ctrl-j"] = key.down
+key["ctrl-k"] = key.up
+key["ctrl-n"] = nil
