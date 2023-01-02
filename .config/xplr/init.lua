8fccc81f586e9508ccc40901c5e415ecbe413f34
Maxim Baz <git@maximbaz.com>
2023-01-02 22:43:32 +0100

    xplr: add wl-clipboard plugin

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -5,6 +5,7 @@ package.path = os.getenv("XDG_CONFIG_HOME") .. "/xplr/?/init.lua"
 require("ui").setup()
 require("keys").setup()
 require("visit-path").setup()
-
-package.path = os.getenv("XDG_CONFIG_HOME") .. "/xplr/?/src/init.lua"
 require("trash-cli").setup()
+require("wl-clipboard").setup{
+  copy_command = "wl-copy -t text/uri-list"
+}
