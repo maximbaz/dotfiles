f3051741100e8fa7a55fdc58e0a4a7e3be9f577f
Maxim Baz <git@maximbaz.com>
2024-05-23 19:26:05 +0200

    add en_DK locale

--- nix/nixosConfigurations/default.nix
+++ nix/nixosConfigurations/default.nix
@@ -15,7 +15,13 @@
   # sound.enable = true;
 
   time.timeZone = "Europe/Copenhagen";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_DK.UTF-8/UTF-8"
+    ];
+  };
 
   networking = {
     hostName = "home-manitoba";
