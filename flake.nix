84a47735fc305512bd821edf58e90bd01e882fc3
Maxim Baz <git@maximbaz.com>
2024-06-01 12:26:32 +0200

    nix: switch back to apple-silicon repo, add private flake

--- flake.nix
+++ flake.nix
@@ -3,14 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    # apple-silicon-support.url = "github:tpwrules/nixos-apple-silicon";
-    apple-silicon-support.url = "github:flokli/nixos-apple-silicon/89a8f646019c6990bca14b55104afa1817dca7d5";
-    apple-silicon-support.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    apple-silicon-support = {
+      url = "github:tpwrules/nixos-apple-silicon";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    maximbaz-private = {
+      url = "git+file:///home/maximbaz/.dotfiles-private";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, apple-silicon-support, ... }: {
+  outputs = { nixpkgs, home-manager, apple-silicon-support, maximbaz-private, ... }: {
     nixosConfigurations = {
       home-manitoba =
         let system = "aarch64-linux";
@@ -19,6 +29,7 @@
           modules = [
             apple-silicon-support.nixosModules.apple-silicon-support
             home-manager.nixosModules.home-manager
+            maximbaz-private.nixosModules.default
             ./nix/default.nix
           ];
         };
