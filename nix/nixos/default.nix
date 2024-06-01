d9b8c25e1da436b90884b3f28027b1027fb46adb
Maxim Baz <git@maximbaz.com>
2024-06-01 14:42:55 +0200

    extract overlay

--- nix/nixos/default.nix
+++ nix/nixos/default.nix
@@ -1,19 +1,4 @@
-{ pkgs, ... }:
-
-let
-  overlay = self: super: {
-    input-fonts = super.input-fonts.overrideAttrs (oldAttrs: {
-      src = super.fetchzip {
-        # This URL is too long for fetchzip, and returns non-reproducible zips with new sha256 every time ☹️
-        # url = "https://input.djr.com/build/?customize&fontSelection=fourStyleFamily&regular=InputMonoNarrow-Regular&italic=InputMonoNarrow-Italic&bold=InputMonoNarrow-Bold&boldItalic=InputMonoNarrow-BoldItalic&a=0&g=0&i=serifs_round&l=serifs_round&zero=slash&asterisk=height&braces=0&preset=default&line-height=1.1&accept=I+do&email=";
-        url = "https://maximbaz.com/input-fonts.zip";
-        sha256 = "09qfb3h2s1dlf6kn8d4f5an6jhfpihn02zl02sjj26zgclrp6blc";
-        stripRoot = false;
-      };
-    });
-  };
-in
-{
+{ pkgs, ... }: {
   boot = {
     loader = {
       systemd-boot.enable = true;
@@ -77,7 +62,7 @@ in
       allowUnfree = true;
       input-fonts.acceptLicense = true;
     };
-    overlays = [ overlay ];
+    overlays = [ (import ../overlay) ];
   };
 
   users.users.maximbaz = {
