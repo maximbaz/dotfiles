a05fe46b4cfd7b1b4657d37b6227d667654b32ea
Maxim Baz <git@maximbaz.com>
2024-06-01 12:50:02 +0200

    rename some folders

--- nix/default.nix
+++ nix/default.nix
@@ -1,10 +1,10 @@
 {
-  imports = [ ./nixosConfigurations ./hardwareProfiles ];
+  imports = [ ./nixos ./hardware ];
 
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
     backupFileExtension = "backup";
-    users.maximbaz = import ./homeProfiles;
+    users.maximbaz = import ./home;
   };
 }
