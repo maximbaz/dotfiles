3292872dc6d5d277a52d812ba3a31f3f11ee4b47
Maxim Baz <git@maximbaz.com>
2024-06-02 20:55:44 +0200

    migrate us-hyper xkb

--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -22,6 +22,21 @@
 
   services.udiskie.enable = true;
 
+  home.file.".config/xkb/symbols/us-hyper" = {
+    text = ''
+      default partial alphanumeric_keys modifier_keys
+
+      xkb_symbols "basic" {
+          include "us"
+
+          replace key <CAPS> { [ Hyper_L ] };
+          replace key <LWIN> { [ Super_L ] };
+          modifier_map Mod4 { <META>, Hyper_L };
+          modifier_map Mod3 { <META>, Super_L };
+      };
+    '';
+  };
+
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
