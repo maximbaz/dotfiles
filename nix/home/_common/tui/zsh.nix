38f2bdf765af5072cec62a64867a4eb8decf44ef
Maxim Baz <git@maximbaz.com>
2024-06-18 18:43:46 +0200

    zsh: simplify linking files

--- nix/home/_common/tui/zsh.nix
+++ nix/home/_common/tui/zsh.nix
@@ -1,10 +1,10 @@
 {
   home.file = {
-    ".p10k-ascii-8color.zsh".text = builtins.readFile ./zsh/.p10k.zsh;
-    ".p10k.zsh".text = builtins.readFile ./zsh/.p10k.zsh;
-    ".zprofile".text = builtins.readFile ./zsh/.zprofile;
-    ".zsh-aliases".text = builtins.readFile ./zsh/.zsh-aliases;
-    ".zshenv".text = builtins.readFile ./zsh/.zshenv;
-    ".zshrc".text = builtins.readFile ./zsh/.zshrc;
+    ".p10k-ascii-8color.zsh".source = ./zsh/.p10k.zsh;
+    ".p10k.zsh".source = ./zsh/.p10k.zsh;
+    ".zprofile".source = ./zsh/.zprofile;
+    ".zsh-aliases".source = ./zsh/.zsh-aliases;
+    ".zshenv".source = ./zsh/.zshenv;
+    ".zshrc".source = ./zsh/.zshrc;
   };
 }
