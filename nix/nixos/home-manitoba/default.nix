88e1e578f15da3813c9e907deefbce43534bdd3e
Maxim Baz <git@maximbaz.com>
2024-06-18 13:56:09 +0200

    free up /boot

--- nix/nixos/home-manitoba/default.nix
+++ nix/nixos/home-manitoba/default.nix
@@ -14,8 +14,11 @@
 
   boot = {
     loader = {
-      systemd-boot.enable = true;
       efi.canTouchEfiVariables = false;
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 10;
+      };
     };
     extraModprobeConfig = ''
       options hid_apple swap_opt_cmd=1 swap_fn_leftctrl=1 iso_layout=1
