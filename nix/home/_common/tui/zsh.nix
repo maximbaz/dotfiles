0226e82d66ce5e35ff8661708b5f8951d2548987
Maxim Baz <git@maximbaz.com>
2024-06-18 14:34:43 +0200

    migrate p10k config

--- nix/home/_common/tui/zsh.nix
+++ nix/home/_common/tui/zsh.nix
@@ -1,5 +1,7 @@
 {
   home.file = {
+    ".p10k-ascii-8color.zsh".text = builtins.readFile ./zsh/.p10k.zsh;
+    ".p10k.zsh".text = builtins.readFile ./zsh/.p10k.zsh;
     ".zprofile".text = builtins.readFile ./zsh/.zprofile;
     ".zsh-aliases".text = builtins.readFile ./zsh/.zsh-aliases;
     ".zshenv".text = builtins.readFile ./zsh/.zshenv;
