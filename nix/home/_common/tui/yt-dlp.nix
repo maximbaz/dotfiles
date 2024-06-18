d6fefdcbdc0c38795058545146f2749e0e2c5195
Maxim Baz <git@maximbaz.com>
2024-06-18 22:22:05 +0200

    yt-dlp: don't embed thumbnail as it requires format change

--- nix/home/_common/tui/yt-dlp.nix
+++ nix/home/_common/tui/yt-dlp.nix
@@ -3,7 +3,6 @@
     enable = true;
     settings = {
       embed-metadata = true;
-      embed-thumbnail = true;
       sponsorblock-mark = "all";
       downloader = lib.getExe pkgs.aria2;
     };
