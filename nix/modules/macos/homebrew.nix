e917a2843b2f7e6b0249a1801a4d6663f3da8eef
Maxim Baz <git@maximbaz.com>
2024-07-23 23:48:09 +0200

    homebrew: auto-install, adjust activation

--- nix/modules/macos/homebrew.nix
+++ nix/modules/macos/homebrew.nix
@@ -1,6 +1,19 @@
 {
+  system.activationScripts.preUserActivation.text = ''
+    if ! xcode-select --version 2>/dev/null; then
+      run xcode-select --install
+    fi
+    if ! /opt/homebrew/bin/brew --version 2>/dev/null; then
+      run /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+  '';
+
   homebrew = {
     enable = true;
+    onActivation = {
+      cleanup = "zap";
+      upgrade = true;
+    };
     casks = [
       "bluesnooze"
       "browserosaurus"
