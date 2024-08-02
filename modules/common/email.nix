99361e9879cd95a247a3f9c0f7d53c8445f24335
Maxim Baz <git@maximbaz.com>
2024-08-02 21:16:35 +0200

    aerc: add notmuch account

--- modules/common/email.nix
+++ modules/common/email.nix
@@ -166,7 +166,19 @@
         mbsync.enable = true;
       };
 
-      accounts.email.maildirBasePath = ".mail";
+      accounts.email = {
+        maildirBasePath = ".mail";
+        accounts.notmuch = {
+          realName = "notmuch";
+          address = "notmuch@localhost";
+          aerc = {
+            enable = true;
+            extraAccounts.source = "notmuch://~/.mail";
+            extraBinds.messages."r" = '':reply -aqA {{index (.Filename | split ("/")) 4}}<Enter>'';
+            extraBinds.view."r" = '':reply -aqA {{index (.Filename | split ("/")) 4}}<Enter>'';
+          };
+        };
+      };
 
       services = {
         imapnotify.enable = true;
