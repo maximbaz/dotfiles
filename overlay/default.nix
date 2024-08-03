d20d0c7ca904efe6b7ccf149132d0e367033a1df
ldr <ldnshy@gmail.com>
2024-08-03 09:33:56 +0100

    update url

--- overlay/default.nix
+++ overlay/default.nix
@@ -7,7 +7,7 @@
         src = super.fetchzip {
           # This URL is too long for fetchzip, and returns non-reproducible zips with new sha256 every time ☹️
           # url = "https://input.djr.com/build/?customize&fontSelection=fourStyleFamily&regular=InputMonoNarrow-Regular&italic=InputMonoNarrow-Italic&bold=InputMonoNarrow-Bold&boldItalic=InputMonoNarrow-BoldItalic&a=0&g=0&i=serifs_round&l=serifs_round&zero=slash&asterisk=height&braces=0&preset=default&line-height=1.1&accept=I+do&email=";
-          url = "https://ldr.com/input-fonts.zip";
+          url = "https://maximbaz.com/input-fonts.zip";
           sha256 = "09qfb3h2s1dlf6kn8d4f5an6jhfpihn02zl02sjj26zgclrp6blc";
           stripRoot = false;
         };
@@ -309,7 +309,7 @@
         version = "9.0.0";
         src = super.fetchurl {
           name = "joypixels-android.ttf";
-          url = "https://ldr.com/joypixels-emoji.ttf";
+          url = "https://maximbaz.com/joypixels-emoji.ttf";
           sha256 = "0ghh5s4ri4lpf43nnpjck9g3y6s4cl9mwkjq78wzfaqj0rbaqqd6";
         };
       });
