be101a6cb5f84f6bb5eac06ac2cf996efea8b1cb
ldr <ldnshy@gmail.com>
2024-08-03 10:49:07 +0100

    fix homebrew install

--- modules/macos/homebrew.nix
+++ modules/macos/homebrew.nix
@@ -1,10 +1,10 @@
 {
   system.activationScripts.preUserActivation.text = ''
     if ! xcode-select --version 2>/dev/null; then
-      run xcode-select --install
+      xcode-select --install
     fi
     if ! /opt/homebrew/bin/brew --version 2>/dev/null; then
-      run /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
   '';
 
