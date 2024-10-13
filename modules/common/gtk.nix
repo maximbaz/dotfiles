82af904eaefbf785224e3a30e1f0973cd017624a
Maxim Baz <git@maximbaz.com>
2024-10-14 00:22:44 +0200

    gtk: fix dark theme

--- modules/common/gtk.nix
+++ modules/common/gtk.nix
@@ -1,7 +1,8 @@
 { config, ... }: {
-  home-manager.users.${config.user}.gtk = {
-    enable = true;
-    gtk3.extraConfig = { gtk-application-prefer-dark-theme = true; };
-    gtk4.extraConfig = { gtk-application-prefer-dark-theme = true; };
+  home-manager.users.${config.user} = {
+    gtk.enable = true;
+    gtk.gtk3.extraConfig = { gtk-application-prefer-dark-theme = true; };
+    gtk.gtk4.extraConfig = { gtk-application-prefer-dark-theme = true; };
+    dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
   };
 }
