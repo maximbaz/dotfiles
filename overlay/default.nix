5849f2404994a8efaa20d5e22ef2a5256ec184c6
Maxim Baz <git@maximbaz.com>
2024-11-23 10:21:21 +0100

    signal: update

--- overlay/default.nix
+++ overlay/default.nix
@@ -54,13 +54,11 @@
 
       signal-desktop = super.signal-desktop.overrideAttrs (_old: rec {
         dir = "Signal";
-        version = "7.33.0";
+        version = "7.34.0";
 
         src = super.fetchurl {
-          # url = "https://github.com/0mniteck/Signal-Desktop-Mobian/raw/${version}/builds/release/signal-desktop_${version}_arm64.deb";
-          # hash = "sha256-2PwEPwQkoNrnSI00CVeTjF7QvxQb9NxQqrcOVisWwFU=";
-          url = "https://maximbaz.com/signal-desktop_${version}_arm64.deb";
-          hash = "sha256-hlQ7gEP2sqgoSQL+VrZTvASgZWfLmb1PfHR2caUPkJ8=";
+          url = "https://github.com/0mniteck/Signal-Desktop-Mobian/raw/${version}/builds/release/signal-desktop_${version}_arm64.deb";
+          hash = "sha256-feNjNhKGIJsV6LH2mKAXd7TEnmvcKXheXmqJZEBqXvE=";
 
           recursiveHash = true;
           downloadToTemp = true;
