1ee39e64bae32a19e5ed892570c0aa482866bd7f
Maxim Baz <git@maximbaz.com>
2024-12-08 20:36:46 +0100

    network-dmenu: add dns servers toggle

--- modules/linux/network-dmenu.nix
+++ modules/linux/network-dmenu.nix
@@ -4,6 +4,18 @@
     xdg.configFile."network-dmenu/config.toml".text = ''
       dmenu_cmd = "dmenu"
       dmenu_args = "-f --no-multi"
+
+      [[actions]]
+      display = "📡 DNS: Use DHCP server"
+      cmd = "sudo resolvectl revert wlan0 && notify-send -i network-wired-no-route 'network-dmenu' 'Using DNS server as set by DHCP'"
+
+      [[actions]]
+      display = "📡 DNS: Use 1.1.1.1 server"
+      cmd = "sudo resolvectl dns wlan0 '1.1.1.1#cloudflare-dns.com' && sudo resolvectl dnsovertls wlan0 yes && notify-send -i network-wired-no-route 'network-dmenu' 'Using DNS server: 1.1.1.1'"
+
+      [[actions]]
+      display = "📡 DNS: Use ControlD server"
+      cmd = "sudo resolvectl dns wlan0 '76.76.2.11#x-hagezi-pro.freedns.controld.com' && sudo resolvectl dnsovertls wlan0 yes && notify-send -i network-wired-no-route 'network-dmenu' 'Using DNS server: ControlD'"
     '';
   };
 }
--- modules/linux/sudo.nix
+++ modules/linux/sudo.nix
@@ -10,6 +10,9 @@
     extraRules = [{
       groups = [ "wheel" ];
       commands = [
+        { command = "/run/current-system/sw/bin/resolvectl dnsovertls wlan0 yes"; options = [ "SETENV" "NOPASSWD" ]; }
+        { command = "/run/current-system/sw/bin/resolvectl dns wlan0 *"; options = [ "SETENV" "NOPASSWD" ]; }
+        { command = "/run/current-system/sw/bin/resolvectl revert wlan0"; options = [ "SETENV" "NOPASSWD" ]; }
         { command = "/run/current-system/sw/bin/systemctl stop pcscd.service"; options = [ "SETENV" "NOPASSWD" ]; }
       ];
     }];
