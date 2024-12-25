a184e3f2501467dae38af5d7251457c6f513999d
Maxim Baz <git@maximbaz.com>
2024-12-25 17:53:51 +0100

    git: return to delta

--- modules/common/git.nix
+++ modules/common/git.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }: {
   home-manager.users.${config.user} = {
     home.packages = with pkgs; [
-      difftastic
       mergiraf
       tig
     ];
@@ -44,9 +43,26 @@
         signByDefault = true;
       };
 
-      difftastic = {
+      delta = {
         enable = true;
-        display = "inline";
+        options = {
+          features = "hyperlinks";
+          file-added-label = "[+]";
+          file-copied-label = "[C]";
+          file-decoration-style = "yellow ul";
+          file-modified-label = "[M]";
+          file-removed-label = "[-]";
+          file-renamed-label = "[R]";
+          file-style = "yellow bold";
+          hunk-header-decoration-style = "omit";
+          hunk-header-style = "syntax italic #303030";
+          minus-emph-style = "syntax bold #780000";
+          minus-style = "syntax #400000";
+          plus-emph-style = "syntax bold #007800";
+          plus-style = "syntax #004000";
+          syntax-theme = "gruvbox-dark";
+          width = 1;
+        };
       };
 
       extraConfig = {
