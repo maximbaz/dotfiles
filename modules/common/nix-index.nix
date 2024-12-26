82f815f743d92e0ce5eeea46e91622148866ed09
Maxim Baz <git@maximbaz.com>
2024-12-26 15:10:34 +0100

    nix-index: remove obsolete workaround

--- modules/common/nix-index.nix
+++ modules/common/nix-index.nix
@@ -1,7 +1,6 @@
-{ config, nix-index-with-small-db, ... }: {
+{ config, ... }: {
   home-manager.users.${config.user} = {
     programs.nix-index.enable = true;
     programs.nix-index-database.comma.enable = true;
-    programs.nix-index.package = nix-index-with-small-db;
   };
 }
