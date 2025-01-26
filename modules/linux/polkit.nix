5753adb27799c06315e3b22b5232f0da39f4c640
Maxim Baz <git@maximbaz.com>
2025-01-27 00:48:46 +0100

    fix polkit agent

--- modules/linux/polkit.nix
+++ modules/linux/polkit.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, lib, util, ... }: {
+{ config, pkgs, util, ... }: {
   home-manager.users.${config.user} = {
     systemd.user.services.polkit-gnome-authentication-agent-1 = util.systemdService {
       Description = "polkit-gnome-authentication-agent-1";
-      ExecStart = lib.getExe' pkgs.polkit_gnome "polkit-gnome-authentication-agent-1";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
     };
 
     home.packages = [ pkgs.polkit_gnome ];
