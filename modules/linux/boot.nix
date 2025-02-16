2c664cbe5335c2813a66b971206a8c1384ac2ee8
Maxim Baz <git@maximbaz.com>
2025-02-16 20:23:31 +0100

    tmpfs: increase inode size

--- modules/linux/boot.nix
+++ modules/linux/boot.nix
@@ -1,6 +1,18 @@
-{
-  boot.tmp = {
-    useTmpfs = true;
-    tmpfsSize = "90%";
-  };
+{ lib, ... }: {
+  systemd.mounts = [{
+    what = "tmpfs";
+    where = "/tmp";
+    type = "tmpfs";
+    mountConfig.Options = lib.concatStringsSep "," [
+      "mode=1777"
+      "strictatime"
+      "rw"
+      "nosuid"
+      "nodev"
+      "size=50G"
+
+      # increase inodes, boot.tmp.useTmpfs cannot do this
+      "nr_inodes=5000000"
+    ];
+  }];
 }
