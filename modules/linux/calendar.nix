427f50917679cbee1ca35008925a40957209bbbf
Max Baz <max@baz.nu>
2025-09-01 10:47:21 +0200

    configure calendar

--- modules/linux/calendar.nix
+++ modules/linux/calendar.nix
@@ -7,10 +7,23 @@
 
       settings = {
         "app.update.auto" = false;
+        "calendar.alarms.eventalarmlen" = 0;
+        "calendar.alarms.onforevents" = 1;
+        "calendar.alarms.onfortodos" = 1;
+        "calendar.alarms.playsound" = false;
+        "calendar.alarms.todoalarmlen" = 0;
+        "calendar.event.defaultlength" = 30;
+        "calendar.events.defaultActionEdit" = true;
+        "calendar.item.editInTab" = true;
+        "calendar.task.defaultdueoffset" = 0;
+        "calendar.task.defaultdue" = "offsetcurrent";
+        "calendar.timezone.local" = "Europe/Copenhagen";
+        "calendar.timezone.useSystemTimezone" = true;
+        "calendar.view.visiblehours" = 14;
         "mail.biff.play_sound" = false;
         "mail.biff.show_alert" = false;
-        "mail.shell.checkDefaultClient" = false;
         "mailnews.start_page.enabled" = false;
+        "mail.shell.checkDefaultClient" = false;
         "privacy.donottrackheader.enabled" = true;
       };
     };
